47d2962ca2ef351f99db5397785667923342f853
Saepudin <saepudinjeh@gmail.com>
2021-05-16 15:21:01 +0700

    update service worker running offline

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,8 @@ const CONFIG = {
     KEY: "de60fe37c22fcff43c9b08133cd6fdfe",
     BASE_URL: "https://api.themoviedb.org/3/",
     BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
-    DEFAULT_LANGUAGE: "en-us"
+    DEFAULT_LANGUAGE: "en-us",
+    CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
